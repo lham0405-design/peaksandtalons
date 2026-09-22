b2238e5e60e9678b3fa349daa2386b06a2d1d19e
lham0405-design <lham0405@gmail.com>
2026-09-22 19:41:51 -0400

    Add eagle attributes and high mountain spawn placement

---
+++ src/main/java/com/leigh/peaksandtalons/event/ModEvents.java
@@ -0,0 +1,26 @@
+package com.leigh.peaksandtalons.event;
+
+import com.leigh.peaksandtalons.PeaksAndTalons;
+import com.leigh.peaksandtalons.entity.EagleEntity;
+import com.leigh.peaksandtalons.registry.ModEntities;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
+
+@EventBusSubscriber(modid = PeaksAndTalons.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+public final class ModEvents {
+    @SubscribeEvent
+    public static void attributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.EAGLE.get(), EagleEntity.attributes().build());
+    }
+
+    @SubscribeEvent
+    public static void spawns(RegisterSpawnPlacementsEvent event) {
+        event.register(ModEntities.EAGLE.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+            (type, level, reason, pos, random) -> pos.getY() >= 140 && level.getMaxLocalRawBrightness(pos) > 8,
+            RegisterSpawnPlacementsEvent.Operation.REPLACE);
+    }
+}
