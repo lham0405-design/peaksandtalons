0cfe41de079b71124cbaae7e00c0730313c70e97
lham0405-design <lham0405@gmail.com>
2026-09-22 19:41:07 -0400

    Add seven necklace socket types

---
+++ src/main/java/com/leigh/peaksandtalons/item/SocketType.java
@@ -0,0 +1,11 @@
+package com.leigh.peaksandtalons.item;
+
+public enum SocketType {
+    DIAMOND,
+    EMERALD,
+    REDSTONE,
+    MAGMA_CREAM,
+    PRISMARINE,
+    EYE_OF_THE_EAGLE,
+    DRAGONS_EGG
+}
