7528ddf2f4bb98b096c2d0cefd821f369cf69fd2
lham0405-design <lham0405@gmail.com>
2026-09-25 15:47:25 -0400

    Remove obsolete Troll Heart creative tab reference

--- src/main/java/com/leigh/peaksandtalons/registry/ModCreativeTab.java
+++ src/main/java/com/leigh/peaksandtalons/registry/ModCreativeTab.java
@@ -13,11 +13,11 @@ public final class ModCreativeTab {
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MAIN = TABS.register("main", () -> CreativeModeTab.builder()
         .title(Component.translatable("itemGroup.peaksandtalons")).icon(() -> new ItemStack(ModItems.EYE_OF_THE_EAGLE.get()))
         .displayItems((parameters, output) -> {
-            output.accept(ModItems.EAGLE_SPAWN_EGG.get()); output.accept(ModItems.EAGLE_FEATHER.get()); output.accept(ModItems.EAGLE_TALON.get());
+            output.accept(ModItems.EAGLE_SPAWN_EGG.get()); output.accept(ModItems.ORDUK_SPAWN_EGG.get()); output.accept(ModItems.EAGLE_FEATHER.get()); output.accept(ModItems.EAGLE_TALON.get());
             output.accept(ModItems.CRUSHED_TALON.get()); output.accept(ModItems.TALON_DUST.get()); output.accept(ModItems.ANCIENT_TALON.get());
             output.accept(ModItems.TALON_BLADE.get()); output.accept(ModItems.TALONSPIRE.get()); output.accept(ModItems.TALONTEER.get());
             output.accept(ModItems.EYE_OF_THE_EAGLE.get()); output.accept(ModItems.STORM_CRYSTAL.get()); output.accept(ModItems.EYE_OF_THE_SUMMIT.get());
-            output.accept(ModItems.GOLDEN_EAGLE_HARNESS.get()); output.accept(ModItems.TROLL_HEART.get());
+            output.accept(ModItems.GOLDEN_EAGLE_HARNESS.get()); output.accept(ModItems.HEART_OF_ORDUK.get());
             output.accept(ModItems.IRON_CHAIN.get()); output.accept(ModItems.GOLD_CHAIN.get()); output.accept(ModItems.NETHERITE_CHAIN.get());
             output.accept(ModItems.REDSTONE_SOCKET.get()); output.accept(ModItems.DRAGONS_EGG_SOCKET.get());
             ModItems.NECKLACES.values().forEach(item -> output.accept(item.get()));
