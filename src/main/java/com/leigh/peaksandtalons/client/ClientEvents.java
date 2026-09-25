464ce95d99b848ddd16beb176fed325a931aee78
lham0405-design <lham0405@gmail.com>
2026-09-24 20:02:37 -0400

    Register Orduk client rendering

--- src/main/java/com/leigh/peaksandtalons/client/ClientEvents.java
+++ src/main/java/com/leigh/peaksandtalons/client/ClientEvents.java
@@ -9,15 +9,13 @@ import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 @EventBusSubscriber(modid = PeaksAndTalons.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public final class ClientEvents {
-    @SubscribeEvent
-    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+    @SubscribeEvent public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ModEntities.EAGLE.get(), EagleRenderer::new);
+        event.registerEntityRenderer(ModEntities.ORDUK.get(), OrdukRenderer::new);
     }
-
-    @SubscribeEvent
-    public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+    @SubscribeEvent public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(EagleModel.LAYER_LOCATION, EagleModel::createBodyLayer);
+        event.registerLayerDefinition(OrdukModel.LAYER_LOCATION, OrdukModel::createBodyLayer);
     }
-
     private ClientEvents() {}
 }
